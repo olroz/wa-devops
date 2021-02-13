5b3de515fbf784ebe6b6dfaa7017a499c6fb6e5d
Ol <ol@gmail.com>
2021-02-13 03:24:09 +0200

    review adjustments

--- OR/configs/cleanup.sh
+++ OR/configs/cleanup.sh
@@ -5,7 +5,7 @@ set -e
 echo 'Cleaning up after bootstrapping...'
 sudo apt-get -y autoremove
 sudo apt-get -y clean
-sudo rm -rf /tmp/*
+sudo rm -r /tmp/*
 cat /dev/null > ~/.bash_history
 #history -c
 exit
